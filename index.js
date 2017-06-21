6cfb4ae14a04141f3783a1413cce5a1402fbb3b8
Robert Pearce <me@robertwpearce.com>
2017-06-21 23:48:53 +1200

    hello world added

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
+// Make our .env configuration file available
 require('dotenv').config()
+
+
+// Import libraries
 const http = require('http')
 const axios = require('axios')
 const { compose, prop } = require('ramda')
 const { json, logger, methods, mount, parseJson, routes } = require('paperplane')
 
-const geocode = req => {
-  return axios({
+
+// Application-specific code
+const hello = req =>
+  Promise
+    .resolve('hello world')
+    .then(json)
+
+const geocode = req =>
+  axios({
     method: 'GET',
     url: 'https://maps.googleapis.com/maps/api/geocode/json',
     params: {
@@ -15,17 +26,24 @@ const geocode = req => {
   })
   .then(prop('data'))
   .then(json)
-}
 
 const endpoints = routes({
+  '/': methods({
+    GET: hello
+  }),
   '/geocode/:address': methods({
     GET: geocode
   })
 })
 
 const app = compose(endpoints, parseJson)
+
+
+// Server options
 const opts = { errLogger: logger, logger }
 const port = process.env.PORT || 3000
 const listening = err => err ? console.error(err) : console.info(`Listening on port: ${port}`)
 
+
+// Start the server
 http.createServer(mount(app, opts)).listen(port, listening)
